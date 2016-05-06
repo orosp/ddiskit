d203dfd257cffafc2e8032aa8eb86e1649bcbd6c
Petr Oros <poros@redhat.com>
2016-05-06 16:27:18 +0200

    prepare_sources command implementation
    Signed-off-by: Petr Oros <poros@redhat.com>

--- ddiskit.py
+++ ddiskit.py
@@ -7,14 +7,43 @@
 #
 # This software may be freely redistributed under the terms of the GNU
 # General Public License version 2 (GPLv2).
+import os
 import sys
 from pprint import pprint
 
 class Ddiskit:
     def cmd_prepare_sources(self, args, configs):
-        # touch config example
-        # create dirs for rpm build
-        print "cmd_prepare_sources"
+        try:
+            sys.stdout.write("Writing module.config ... ")
+            if os.path.isfile("module.config"):
+                sys.stdout.write("File Exist")
+            else:
+                with open('config', 'r') as fin:
+                    read_data = fin.read()
+                fin.close()
+                fout = open('module.config', 'w')
+                fout.write(read_data)
+                fout.close()
+                sys.stdout.write("OK")
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+        except IOError as e:
+            sys.stdout.write(e.strerror)
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+
+        sys.stdout.write("Creating directory structure for RPM build ... ")
+        try:
+            os.makedirs("rpm")
+            os.makedirs("rpm/SPEC")
+            os.makedirs("rpm/SOURCE")
+        except OSError as e:
+            sys.stdout.write(e.strerror)
+        else:
+            sys.stdout.write("OK")
+        sys.stdout.write("\n")
+        sys.stdout.write("Done\n")
+        sys.stdout.flush()
 
     def cmd_generate_spec(self, args, configs):
         # parse config file and create spec file(s)
