4d830125cf951025db06e0a6646e8ddb15731967
Petr Oros <poros@redhat.com>
2016-05-09 10:39:06 +0200

    Add setup and move code to src dir
    Signed-off-by: Petr Oros <poros@redhat.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+from setuptools import setup, Command
+
+setup(
+    name = "ddiskit",
+    version = "3.0",
+    author = "Petr Oros",
+    author_email = "poros@redhat.com",
+    description = ("Red Hat tool for create Driver Update Disk"),
+    license = "GPLv2+",
+    url = "http://git.engineering.redhat.com/git/users/poros/ddiskit.git/",
+    package_dir = {'': 'src'},
+    packages = ['ddiskit'],
+    scripts = ['src/bin/ddiskit'],
+    data_files = [('/etc/bash_completion.d', ['src/ddiskit.bash'])],
+)
