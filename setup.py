1149a61ea22c0e896f89c68b06998cd6c44a10c8
Eugene Syromiatnikov <esyr@redhat.com>
2017-04-06 16:47:16 +0200

    setup.py: whitespace cleanup, as suggested by flake8

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup
 
 setup(
-    name = "ddiskit",
-    version = "3.2",
-    author = "Petr Oros",
-    author_email = "poros@redhat.com",
-    description = ("Red Hat tool for create Driver Update Disk"),
-    license = "GPLv2+",
-    url = "http://git.engineering.redhat.com/git/users/poros/ddiskit.git/",
-    packages = ['ddiskit'],
+    name="ddiskit",
+    version="3.2",
+    author="Petr Oros",
+    author_email="poros@redhat.com",
+    description=("Red Hat tool for create Driver Update Disk"),
+    license="GPLv2+",
+    url="http://git.engineering.redhat.com/git/users/poros/ddiskit.git/",
+    packages=['ddiskit'],
     package_dir={'ddiskit': 'src/'},
-    data_files = [('/etc/bash_completion.d', ['ddiskit.bash']),
-                  ('/usr/share/ddiskit/templates', ['templates/spec']),
-                  ('/usr/share/ddiskit/templates', ['templates/config'])],
+    data_files=[('/etc/bash_completion.d', ['ddiskit.bash']),
+                ('/usr/share/ddiskit/templates', ['templates/spec']),
+                ('/usr/share/ddiskit/templates', ['templates/config'])],
     entry_points={
         'console_scripts': [
             'ddiskit=ddiskit.ddiskit:main',
