e635cea9d8068d0e16d286523fe29d3f98bf4989
Eugene Syromiatnikov <esyr@redhat.com>
2017-04-06 16:47:20 +0200

    src: update copyright years

--- src/ddiskit.py
+++ src/ddiskit.py
@@ -3,7 +3,7 @@
 # ddiskit - Red Hat tool for create Driver Update Disk
 #
 # Author: Petr Oros <poros@redhat.com>
-# Copyright (C) 2016 Red Hat, Inc.
+# Copyright (C) 2016-2017 Red Hat, Inc.
 #
 # This software may be freely redistributed under the terms of the GNU
 # General Public License version 3 (GPLv3).
